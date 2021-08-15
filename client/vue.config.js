1d832f1fda14ebc777e21d8e87045b9cb17d544d
wxl <570937272@qq.com>
2021-08-15 08:24:56 -0700

    开发环境将请求代理到后台3000端口

--- client/vue.config.js
+++ client/vue.config.js
@@ -36,7 +36,13 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    before: require('./mock/mock-server.js')
+    // before: require('./mock/mock-server.js'),
+    proxy: {
+      '.*': {
+        target: 'http://localhost:3000',
+        ws: true
+      }
+    }
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that

f9f47725071583ae098503df545e270956fe14bb
wxl <570937272@qq.com>
2021-10-21 11:17:07 +0800

    a little change

---
+++ client/src/views/chat/friend/AddFriendDialog.vue
@@ -0,0 +1,288 @@
+<template>
+  <el-dialog class="mydialog" :visible.sync="dialogVisible" @open="handleDialogOpen" :modal="false">
+    <div class="body-left">
+      <el-upload
+        class="avatar-uploader"
+        action="/user/avatar"
+        :show-file-list="false"
+        :headers="authHeader"
+        :on-success="handleAvatarSuccess"
+        :before-upload="beforeAvatarUpload"
+      >
+        <img v-if="avatarUrl" :src="avatarUrl" class="avatar">
+        <i v-else class="el-icon-plus avatar-uploader-icon" />
+      </el-upload>
+    </div>
+    <el-divider direction="vertical"></el-divider>
+    <div class="body-right">
+      <el-form ref="userInfo" :model="userInfo">
+        <el-form-item class="edit-button-item">
+          <div class="edit-button">
+            <el-button type="text" v-show="isEdit" @click="handleCancel">取消</el-button>
+            <el-button type="text" @click="handleEdit">{{ isEdit ? "保存" : "编辑" }}</el-button>
+          </div>
+        </el-form-item>
+        <el-form-item class="first-item">
+          <i class="fa fa-user-o fa-fw" />
+          <el-input 
+            v-if="isEdit"
+            v-model="userInfo.username" 
+            placeholder="用户名"
+            tabindex="1"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.username}">{{ userInfo.username ? userInfo.username : '用户名' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-tags fa-fw" />
+          <el-input 
+            v-if="isEdit"
+            v-model="userInfo.signature" 
+            placeholder="个性签名"
+            tabindex="2"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.signature}">{{ userInfo.signature ? userInfo.signature : '个性签名' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-birthday-cake fa-fw" />
+          <el-date-picker 
+            v-if="isEdit"
+            v-model="userInfo.birth_date"
+            type="date" 
+            placeholder="出生日期" 
+            tabindex="3"
+            prefix-icon=""  
+            format="yyyy-MM-dd"
+            value-format="yyyy-MM-dd"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.birth_date}">{{ userInfo.birth_date ? userInfo.birth_date : '出生日期' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-briefcase fa-fw" />
+          <el-select 
+            v-if="isEdit" 
+            v-model="userInfo.job" 
+            filterable
+            allow-create
+            placeholder="职业" 
+            tabindex="4">
+            <el-option value="工人" />
+            <el-option value="农民" />
+          </el-select>
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.job}">{{ userInfo.job ? userInfo.job : '工作' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-map-marker fa-lg fa-fw" />
+          <el-input 
+            v-if="isEdit"
+            v-model="userInfo.address" 
+            placeholder="现居住地"
+            tabindex="5"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.address}">{{ userInfo.address ? userInfo.address : '现居住地' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-mobile fa-lg fa-fw" />
+          <el-input
+            v-if="isEdit" 
+            v-model="userInfo.phone" 
+            placeholder="电话"
+            tabindex="6"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.phone}">{{ userInfo.phone ? userInfo.phone : '电话' }}</span>
+        </el-form-item>
+        <el-form-item>
+          <i class="fa fa-envelope-o fa-fw" />
+          <el-input 
+            v-if="isEdit"
+            v-model="userInfo.email" 
+            placeholder="邮箱"
+            tabindex="7"
+          />
+          <span v-else :class="{'info-label': true, 'info-label-placeholder': !userInfo.email}">{{ userInfo.email ? userInfo.email : '邮箱' }}</span>
+        </el-form-item>
+      </el-form>
+    </div>
+  </el-dialog>
+</template>
+
+<script>
+import { getToken } from '@/utils/auth'
+import { getInfo, editInfo } from '@/api/user'
+// import PublicMixin from '@/utils/public-mixin'
+
+export default {
+  name: 'UserInfoDialog',
+  props: {
+    userInfoDialogVisible: Boolean
+  },
+  // mixins: [PublicMixin],
+  data() {
+    return {
+      userInfo: {
+        username: '',
+        signature: '',
+        birth_date: '',
+        job: '',
+        address: '',
+        phone: '',
+        email: ''
+      },
+      isEdit: false
+    }
+  },
+  created: function() {
+    document.onkeydown = e => {   if (this.dialogVisible && e.key === 'Escape') {
+        this.dialogVisible = false
+      }
+    }
+  },
+  computed: {
+    dialogVisible: {
+      get: function() {
+        return this.userInfoDialogVisible
+      },
+      set: function(value) {
+        this.$emit('update:user-info-dialog-visible', value)
+      }
+    },
+    authHeader: function() {
+      return { Authorization: 'Bearer ' + getToken() }
+    },
+    avatarUrl: function() {
+      return this.$store.state.user.avatar
+    }
+  },
+  methods: {
+    handleAvatarSuccess(res, file) {
+      this.$store.commit('user/SET_AVATAR', res.data.avatar)
+    },
+    async beforeAvatarUpload(file) {
+      const isLt2M = file.size / 1024 / 1024 < 2
+
+      if (!isLt2M) {
+        this.$message.error('上传头像图片大小不能超过 2MB!')
+        return new Promise.reject(false) //只有这种方式可行
+        // await false
+        // return false
+      }
+
+      await this.$confirm('确定上传图像？', '确认信息', {
+        iconClass: 'fa fa-question-circle'
+      })
+    },
+    handleDialogOpen() {
+      this.isEdit = false
+      getInfo().then(response => {
+        const { data } = response
+        Object.assign(this.userInfo, data)
+      })
+    },
+    handleEdit() {
+      if (!this.isEdit) {
+        this.isEdit = true
+        return
+      } else {
+        editInfo(this.userInfo).then(() => {
+          this.isEdit = false
+          this.handleDialogOpen()
+        })
+      }
+    },
+    handleCancel() {
+      this.isEdit = false
+      this.handleDialogOpen()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+$avatar_size:150px;
+
+  .mydialog ::v-deep {
+    .el-dialog {
+      max-width: 700px;
+      min-width: 550px;
+    }
+    .el-dialog__header {
+      padding-top: 0px;
+    }
+  }  
+
+.body-left {
+  display: inline-block;
+  width: 30%;
+
+  .avatar-uploader {
+    border: 1px dashed #d9d9d9;
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    width: $avatar_size;
+    height: $avatar_size;
+    margin: 110px auto;
+  }
+  .avatar-uploader:hover {
+    border-color: #409EFF;
+  }
+  .avatar-uploader-icon {
+    font-size: 28px;
+    color: #8c939d;
+    width: $avatar_size;
+    height: $avatar_size;
+    line-height: $avatar_size;
+    text-align: center;
+  }
+  .avatar {
+    width: $avatar_size;
+    height: $avatar_size;
+    display: block;
+  }
+}
+
+.body-right {
+  display: inline-block;
+  width: 65%;
+
+  .edit-button {
+    float: right;
+  }
+
+  .el-form-item {
+    margin: 10px;
+    .el-input, .el-date-picker, .el-select {
+      display: inline-block;
+      width: 90%;
+      float: right;
+    }
+
+    i {
+      width: 10%;
+      text-align: center;
+    }
+
+    .info-label {
+      padding-left: 8px;
+      font-size: 1.1em;
+    }
+    .info-label-placeholder {
+      color:#C0C4CC;
+    }
+  }
+  .edit-button-item {
+    margin-top: 0px;
+    margin-bottom: 0px;
+  }
+  .first-item {
+    margin-top: 0px;
+  }
+}
+
+.el-divider {
+  height: 300px;
+  vertical-align: baseline;
+  // top: 20px;
+}
+</style>
--- client/src/views/chat/friend/index.vue
+++ client/src/views/chat/friend/index.vue
@@ -1,23 +1,38 @@
 <template>
   <div class="components-container">
     <div class="left-container column">
+      <div class="left-top">
+        <el-input
+          placeholder="搜索"
+          v-model="search"
+          clearable
+          prefix-icon="el-icon-search"
+          size="small"
+          @input="onSearch"
+          >
+        </el-input>
+        <el-divider direction="vertical"></el-divider>
+        <el-tooltip content="添加好友" placement="right">
+          <el-button icon="el-icon-plus" size="small" @click.native.prevent="addFriend"></el-button>
+        </el-tooltip>
+      </div>
       <friend-item v-for="friend in friends" :id="friend.id" :image-url="friend.avatar" :name="friend.username" :signature="friend.signature" :key="friend.username" :is-clicked="friend.isClicked" @item-click="onFriendClicked"/>
     </div>
     <div v-if="hasDetail" class="right-container column">
       <div class="detail-1">
-        <div class="name-signature column">
-          <p>{{ detail.username }}</p>
-          <p>{{ detail.signature }}</p>
+        <div class="name-signature">
+          <p class="username">{{ detail.username }}</p>
+          <p class="signature">{{ detail.signature }}</p>
         </div>
         <img :src="detail.avatar" alt="暂无头像" class="avatar">
       </div>
       <div class="detail-2">
-        <p><span>备注</span><span>{{ detail.remark }}</span></p>
-        <p><span>电话</span><span>{{ detail.phone }}</span></p>
-        <p><span>邮箱</span><span>{{ detail.email }}</span></p>
-        <p><span>工作</span><span>{{ detail.job }}</span></p>
-        <p><span>年龄</span><span>{{ detail.birth_date }}</span></p>
-        <p><span>住址</span><span>{{ detail.address }}</span></p>
+        <p><span class="title">备注</span><span>{{ detail.remark }}</span></p>
+        <p><span class="title">电话</span><span>{{ detail.phone }}</span></p>
+        <p><span class="title">邮箱</span><span>{{ detail.email }}</span></p>
+        <p><span class="title">工作</span><span>{{ detail.job }}</span></p>
+        <p><span class="title">年龄</span><span>{{ detail.age }}</span></p>
+        <p><span class="title">住址</span><span>{{ detail.address }}</span></p>
       </div>
     </div>
   </div>
@@ -34,19 +49,48 @@ export default {
   data() {
     return {
       friends: [],
+      friendsBak: [],
       detail: {}, 
-      hasDetail: false
+      hasDetail: false,
+      search: ''
     }
   },
   created() {
     this.getFriends()
   },
   methods: {
+    isLeapYear(year) {
+      if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        return true
+      } else {
+        return false
+      }
+    },
+    getAge(date) {
+      console.log(date)
+      let age = 0
+      let friendDate = new Date(date);
+      let now = new Date()
+      if (now.getMonth() > friendDate.getMonth()) {
+        age = now.getFullYear() - friendDate.getFullYear()
+      } else if (now.getMonth() < friendDate.getMonth()) {
+        age = now.getFullYear() - friendDate.getFullYear() - 1
+      } else {
+        if (now.getDate() >= friendDate.getDate()) {
+          age = now.getFullYear() - friendDate.getFullYear()
+        } else {
+          age = now.getFullYear() - friendDate.getFullYear() - 1
+        }
+      }
+      
+      return age
+    },
     getFriends() {
       getFriendList().then(response => {
         this.friends = response.data.map(friend => {
           return Object.assign({}, friend, { isClicked: false })
         })
+        this.friendsBak = this.friends
       })
     },
     onFriendClicked(id) {
@@ -61,14 +105,28 @@ export default {
 
       getFriendDetail(id).then(res => {
         console.log(res.data)
-        this.detail = res.data
+        this.detail = Object.assign({}, res.data, { age: this.getAge(res.data.birth_date)})
+      })
+    },
+    addFriend() {
+      alert('add friend')
+    },
+    onSearch(str) {
+      console.log(str)
+      this.detail = {}
+      this.hasDetail = false
+      this.friendsBak.forEach(friend => {
+        friend.isClicked = false
+      })
+      this.friends = this.friendsBak.filter(friend => {
+        return friend.username.indexOf(str) !== -1
       })
     }
   }
 }
 </script>
 
-<style  scoped>
+<style lang="scss" scoped>
   .components-container {
     position: relative;
     height: calc(100vh - 50px);
@@ -82,6 +140,17 @@ export default {
     border-right: 1px solid #BDC8D6;
     height: 100%;
     width: 300px;
+
+    .left-top {
+      padding-top: 5px;
+      padding-left: 5px;
+      padding-bottom: 3px;
+      border-bottom: 1px solid lightgray;
+      .el-input {
+        width: 77%;
+      }
+    }
+    
   }
 
   .right-container {
@@ -94,26 +163,53 @@ export default {
 
   .detail-1 {
     padding: 20px;
-    /* background-color: gray; */
-    border-bottom: 1px solid gray;
+    padding-bottom: 0px;
+    border-bottom: 1px solid lightgray;
     width: 400px;
     display: flex;
     justify-content: space-between;
   }
 
-  .detail-2 {
-    padding: 20px;
-    /* background-color: lightgray; */
-    width: 400px;
+  .name-signature {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    p { 
+      margin: 0px;
+    }
+
+    .username {
+      font-size: 2em;
+      margin-bottom: 15px;
+    }
   }
 
   .avatar {
     float: right;
-    width: 120px;
-    height: 120px;
+    width: 150px;
+    height: 150px;
     border-radius: 20px;
     object-fit: contain;
   }
+
+  .detail-2 {
+    padding-left: 30px;
+    padding-right: 30px;
+    padding-top: 5px;
+    width: 400px;
+
+    .title {
+      color: gray;  
+      margin-right: 20px;
+    }
+
+    .el-input {
+      width: 75%;
+    }
+  }
+
+
 
 
 </style>
